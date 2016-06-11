d9c648399ac0a4ee0f219929a93018d06cd2ae16
Melissa Hanson <dialmformelissa@gmail.com>
2016-06-11 14:28:12 -0400

    created index, show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,16 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only: [:show, :edit, :update, :destroy]
+
   def index
     @movies = Movie.all
+  end
+
+  def show
+  end
+
+  private
+
+  def find_movie
+    @movie = Movie.find_by_id(params[:id])
   end
 end
